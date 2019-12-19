91e75c6b991797f42546edbb90cd06696fd02723
CheStix <che.eldar@gmail.com>
2019-12-19 10:49:12 +0300

    posts list change on class based view add pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_list, post_detail, PostListView
 
 app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', post_list, name='post_list_url'),
+    # path('', post_list, name='post_list_url'),
+    path('', PostListView.as_view(), name='post_list_url'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail_url'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 from .models import Post
 
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts on page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page don't integer return first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if the page number is greater than the total number of pages, return the last one
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
