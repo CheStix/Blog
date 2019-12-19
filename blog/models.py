de8deec1a2349fbd1472c2de32d7d549a7e2864e
CheStix <che.eldar@gmail.com>
2019-12-19 14:18:42 +0300

    add model for comments list and adding comments on post page

--- blog/models.py
+++ blog/models.py
@@ -37,3 +37,21 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:post_detail_url', args=[self.publish.year, self.publish.month,
                                                      self.publish.day, self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', verbose_name='Пост')
+    name = models.CharField(max_length=80, verbose_name='Имя')
+    email = models.EmailField(verbose_name='Email')
+    body = models.TextField(verbose_name='Текс')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Изменено')
+    active = models.BooleanField(default=True, verbose_name='Активен')
+
+    class Meta:
+        ordering = ('created',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -32,7 +32,25 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # comments list for current post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # user sent comment
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # create comment but not save
+            new_comment = comment_form.save(commit=False)
+            # join comment with post
+            new_comment.post = post
+            # save comment on base
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'new_comment': new_comment,
+                                                     'comment_form': comment_form})
 
 
 def post_share(request, post_id):
